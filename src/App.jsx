564ffd8781daad3c5c840ac6ddb8c83768f7d2ba
Anany Koranne <ananykoranne2609@gmail.com>
2025-03-08 11:10:29 +0530

    init commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-// src/App.jsx
-
-import React from 'react';
-import Home from './Home';
-import './App.css'; // Import global styles
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home"; // Home.jsx is inside components folder
+import AddBook from "./components/AddBooks"; // Assuming AddBook.jsx is inside pages folder
+import "./App.css"; // App.css is in the same folder
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add-book" element={<AddBook />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,19 @@
-// src/components/Home.jsx
-
 import React from "react";
-import BookCard from "./components/BookCard";
-import books from "./booksData";
-import "./Home.css"; // Import the CSS for styling
+import { Link } from "react-router-dom";
+import BookCard from "./components/BookCard"; // Since BookCard.jsx is in the same folder
+import books from "./booksData"; // booksData.jsx is one level up
+import "./Home.css"; // Home.css is in the same folder
 
 const Home = () => {
   return (
     <div className="home-container">
       <h1>Book Library</h1>
+
+      {/* Add Book Button */}
+      <Link to="/add-book" className="add-book-button">
+        Add Book
+      </Link>
+
       <div className="books-grid">
         {books.map((book) => (
           <BookCard key={book.id} book={book} />
